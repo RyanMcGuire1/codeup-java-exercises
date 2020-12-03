f45a071b09a5630a5a07b369dcd25fa63403f84c
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2020-12-03 15:06:55 -0600

    Do-while loop to count 0 to 100 by 2s

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,18 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
-       int i = 5;
-       while (i <= 15){
-           System.out.print(i + " ");
-           i++;
-       }
+//       int i = 5;
+//       while (i <= 15){
+//           System.out.print(i + " ");
+//           i++;
+//       }
+
+       int two = 0;
+        do{
+            System.out.println(two);
+            two+=2;
+        }
+        while(two <= 100);
+
+        }
     }
-}
+
