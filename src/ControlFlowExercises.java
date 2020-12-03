a1120c3b1951d487ccd29ae4d0c74124afdfcb0a
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2020-12-03 14:00:37 -0600

    Acheived proper output for first exercise (Loops Basics)

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,7 +2,7 @@ public class ControlFlowExercises {
     public static void main(String[] args){
        int i = 5;
        while (i <= 15){
-           System.out.println(i);
+           System.out.print(i + " ");
            i++;
        }
     }
