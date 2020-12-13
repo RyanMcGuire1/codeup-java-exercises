bfe462b20a6bc3501a28a35357e004e0e31c6a5b
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2020-12-12 23:31:03 -0600

    more work on array exercises

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,7 +1,18 @@
+import java.util.Random;
+
 public class ServerNameGenerator {
 
-    String[] adj = {"Noisy","Lame","Awesome","Pretty","Fat","Skinny","Short","Tall","Light","Dark"};
-    String[] noun = {"Duck","Turtle","Slug","Unicorn","Bulldog","Bacon","House","Cat","Ninja","Butterfly"};
+    static String[] adj = {"Noisy","Lame","Awesome","Pretty","Fat","Skinny","Short","Tall","Light","Dark"};
+   static String[] noun = {"Duck","Turtle","Slug","Unicorn","Bulldog","Bacon","House","Cat","Ninja","Butterfly"};
 
+    public static void random(){
+        Random random = new Random();
+        int randomAdjIndex = random.nextInt(adj.length);
+        int randomNounIndex = random.nextInt(noun.length);
+        System.out.printf("Here is your server name: \n%s-%s ", adj[randomAdjIndex], noun[randomNounIndex]);
+    }
 
+    public static void main(String[] args) {
+        random();
+    }
 }
